039d41a791b988baa69aaef5571397a8687a7aee
Jiovany_R_Vincent <jiovanyronaldvincent@macbook-pro-de-jiovany-1.home>
2019-03-23 21:33:40 +0100

    delete ContentFooter.js and add by ContentMenu.js

--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { ContentMenu } from './components/ContentMenu';
 
 export class Menu extends React.Component {
   render() {
     return (
       <div>
-        <h1>Menu page</h1>
+        <ContentMenu />
       </div>
     );
   }
