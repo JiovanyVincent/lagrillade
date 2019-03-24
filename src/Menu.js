b9a3ae9c066215c7907dc5c052ffd5e9dc439ffa
Jiovany_R_Vincent <jiovanyronaldvincent@macbook-pro-de-jiovany-1.home>
2019-03-24 12:27:15 +0100

    put images' list

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { ContentMenu } from './components/ContentMenu';
+import accras from './assets/Images/food-mix/accras.jpg';
+import bbq from './assets/Images/food-mix/bbq.jpg';
+import ribs from './assets/Images/food-mix/PorkRibs.png';
+import montBlanc from './assets/Images/desserts/mont-blanc.jpg';
 
 export class Menu extends React.Component {
   render() {
+    const images = [accras, bbq, ribs, montBlanc];
+    const listImages = images.map((image, i) =>
+                      <div>
+                        <img className="images-menu img-fluid rounded" 
+                        key={"image_" + i} 
+                        src={image} 
+                        alt="" />
+                      </div> )
     return (
       <div>
-        <ContentMenu />
+        <ContentMenu listImage={listImages}/>
       </div>
     );
   }
--- src/components/ContentMenu.js
+++ src/components/ContentMenu.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export const ContentMenu = () => {
+export const ContentMenu = (props) => {
   return (
     <div className="content-menu">
       <h1>Menu Page</h1>
+      <div className="container section-images">
+      <p>{props.listImage}</p>
+      </div>
     </div>
   );
 }
