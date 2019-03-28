8e107fcdbe853239b28b4e85fc7c8b7d59dbaebf
Jiovany_R_Vincent <jiovanyronaldvincent@macbook-pro-de-jiovany-1.home>
2019-03-28 11:34:42 +0100

    Order-Online's skeleton

--- src/OrderOnline.js
+++ src/OrderOnline.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { ContentOrder } from './components/ContentOrder';
 
 export class OrderOnline extends React.Component {
   render() {
     return (
-      <div>
-        <ContentOrder />      
+      <div id="content-order">
+        <div className="left-content-order">
+          <h2>left page</h2>
+        </div>
+        <div className="main-content-order">
+          <h2>main page</h2>
+        </div>
+        <div className="right-content-order">
+          <h2>right page</h2>
       </div>
+    </div>
     );
   }
 }
